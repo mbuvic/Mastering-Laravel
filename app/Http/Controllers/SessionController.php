1c99f792d629c104dd98a38c0c230c5cb3adb76b
mbuvic <charlesmbuvipeter@gmail.com>
2025-03-05 12:59:58 +0300

    Day 22

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -11,6 +13,29 @@ class SessionController extends Controller
     }
 
     public function store() {
-        dd(request()->all());
+        //validate
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        //attempt login
+        if (!Auth::attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Sorry, Your provided credentials did not match our records'
+            ]);
+        }
+
+        //regenerate session token
+        request()->session()->regenerate();
+
+        //redirect
+        return redirect('/jobs');
+
+    }
+
+    public function destroy() {
+        Auth::logout();
+        return redirect('/');
     }
 }
